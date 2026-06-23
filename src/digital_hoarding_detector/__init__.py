8f7262de7af5ec63534f6ac10b3149b0496ce9a4
Maitri Polisetty <maitri.polisetty@gmail.com>
2026-06-23 18:21:06 +0530

    Add screenshot detection

--- src/digital_hoarding_detector/__init__.py
+++ src/digital_hoarding_detector/__init__.py
@@ -13,17 +13,33 @@ from .screenshot import (
     analyze_screenshot,
     analyze_screenshot_batch,
 )
+from .selfie import (
+    FaceBox,
+    SelfieAnalysis,
+    SelfieGroup,
+    detect_faces,
+    extract_face_embedding,
+    face_similarity,
+    find_similar_selfies,
+)
 
 __all__ = [
     "BlurAnalysis",
     "DuplicateAnalysis",
     "DuplicateGroup",
+    "FaceBox",
     "ScreenshotAnalysis",
+    "SelfieAnalysis",
+    "SelfieGroup",
     "analyze_blur",
     "analyze_blur_batch",
     "analyze_screenshot",
     "analyze_screenshot_batch",
     "difference_hash",
+    "detect_faces",
+    "extract_face_embedding",
+    "face_similarity",
     "find_duplicate_groups",
+    "find_similar_selfies",
     "hamming_distance",
 ]
